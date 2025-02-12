92278adc0ca6d1454f28e76f303b9bc7ff3a8633
belladev250 <lissadev17@gmail.com>
2025-02-12 11:27:27 +0200

    font change

--- src/components/SubNavbar.tsx
+++ src/components/SubNavbar.tsx
@@ -11,14 +11,14 @@ const SubNavbar = () => {
         <Link href='/'>
         <Image src='/assets/logo.svg' alt="logo" width={130} height={130} className='flex-shrink-0' />
         </Link>
-        <Link href='/about' className='font-WorkSans font-light text-lg text-white'>About</Link>
-        <Link href='/portfolio' className='font-WorkSans font-light text-md text-white'>Portofolio</Link>
-        <Link href='/services' className='font-WorkSans font-light text-md text-white'>Services</Link>
-        <Link href='/stories' className='font-WorkSans font-light text-md text-white'>Stories</Link>
+        <Link href='/about' className='font-Gothic font-light text-lg text-white'>About</Link>
+        <Link href='/portfolio' className='font-Gothic font-light text-md text-white'>Portofolio</Link>
+        <Link href='/services' className='font-Gothic font-light text-md text-white'>Services</Link>
+        <Link href='/stories' className='font-Gothic font-light text-md text-white'>Stories</Link>
     </div>  
 
     <div className="space-x-6">
-    <Link href='/joinus' className='font-WorkSans font-light text-md text-white'>Join Us </Link>
+    <Link href='/joinus' className='font-Gothic font-light text-md text-white'>Join Us </Link>
     <Link href='/contact'>
     <button className='font-WorkSans text-black font-semibold bg-gold px-4 py-2 text-md cursor-pointer'>
      Contact Us </button>
