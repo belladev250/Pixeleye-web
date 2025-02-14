e7c9fa941da26fd7f73f79b80b6163ce62cb2237
Bethiane <ubethiane07@gmail.com>
2025-02-14 09:57:18 +0200

    contact and about not responsive

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,44 @@
 import React from 'react';
+import Image from "next/image";
 
 const about = () => {
   return (
-    <div>
-      <p>about page</p>
+    <div className="p-24 space-y-64 sm:mt-30">
+
+      <div className=" flex items-center justify-center mt-32">
+
+        <div className=" relative">
+          <Image src="/assets/illustration.svg" alt="eye" width={400} height={400} ></Image>
+        </div>
+
+        <div className="flex flex-col lg:flex-row absolute p-8 lg:p-12 lg:space-x-32 ">
+          <div className="font-Caslon  text-3xl lg:text-6xl w-full lg:w-[30vw] space-y-4">
+            <p>The Creative </p>
+            <p>Impact</p>
+          </div>
+
+          <div className="font-Gothic  text-lg lg:text-md w-full lg:w-[45vw] space-y-6 lg:mt-60  ">
+            <p>Since 2015, we have been a go-to creative agency for entities that seek crafting narratives that move, inspire and transform. Our vision is clear: Helping businesses build impact through innovative storytelling.
+              At Pixel Eye, we give life to creative projects that amplify the client’s vision and mission. Through our services – branding, audio-visual production and campaign management – we shape brand images and build customer relationships that last a lifetime.
+            </p>
+            <h1 className="relative text-gold">WHAT’S YOUR STORY?</h1>
+          </div>
+
+        </div>
+
+      </div>
+      <div className="w-full">
+        <Image
+          src="/assets/about.jpg"
+          alt="camera"
+          width={1500}
+          height={1500}
+          layout="responsive"
+        />
+      </div>
+
     </div>
+
   );
 }
 
